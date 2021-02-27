27626aaa2880791851eba16df3907bd927e23dbe
Suhas K A <suhaska2010@gmail.com>
2021-02-27 22:47:47 +0530

    Update 03_add_digits_in_two_digit_number.py

--- 01_beginner_mode/03_add_digits_in_two_digit_number.py
+++ 01_beginner_mode/03_add_digits_in_two_digit_number.py
@@ -1,9 +1,6 @@
 # Get the 2 digit input number
 two_digit_number = input("Type a two digit number: ")
 
-#Convert the given number to a string
-string_two_digit_number = str(two_digit_number)
-
 #Subscript the string and add those two together
-print(int(string_two_digit_number[0]) + 
-int(string_two_digit_number[1]))
+print(int(two_digit_number[0]) + 
+int(two_digit_number[1]))
