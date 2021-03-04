cf44c90935778835201a4354dbe6bb67bd4f0531
Suhas K A <suhaska2010@gmail.com>
2021-03-05 00:43:02 +0530

    detect duplicate guess and not add penalty

--- 01_beginner_mode/hangman_game/hangman_game.py
+++ 01_beginner_mode/hangman_game/hangman_game.py
@@ -24,7 +24,8 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    if guess in display:
+      print(f"You already guessed {guess}")
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -40,7 +41,7 @@ while not end_of_game:
         print(f"letter {guess} is not in the word")
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print("You lose!!")
             print(f"The word was: {chosen_word}")
 
     #Join all the elements in the list and turn it into a String.
@@ -49,7 +50,7 @@ while not end_of_game:
     #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win.")
+        print("You win!!")
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stage.stages[lives])
